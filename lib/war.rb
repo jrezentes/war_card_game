4ebe6fc401b11db178b5462238cc676a30c4a737
Joseph Rezentes <josephrezentes@gmail.com>
2015-10-09 09:04:41 -0400

    Almost finished developing all base specs for all Subclasses using BDD

--- lib/war.rb
+++ lib/war.rb
@@ -11,11 +11,19 @@ class War
   def play_hand()
     cards_played = []
 
-    @player1.play_card() = player1_current_card
-    @player2.play_card() = player2_current_card
-    cards_played.push(player1_current_card)
-    cards_played.push(player2_current_card)
+    @player1.play_card() = player1_string
+    @player2.play_card() = player2_string
+    player1_current_card = player1_string.split('')
+    player2_current_card = player2_string.split('')
+    cards_played.push(player1_string)
+    cards_played.push(player2_string)
+    player1_current_rank = player1_current_card[0].to_i
+    player2_current_rank = player2_current_card[0].to_i
 
-    if
+    if player1_current_rank > player2_current_rank
+      cards_played.each {|card| card << @player1}
+    elsif player2_current_rank > player1_current_rank
+      cards_played.each {|card| card << @player1}
+    end
   end
 end
